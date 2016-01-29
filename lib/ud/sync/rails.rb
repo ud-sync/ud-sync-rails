597ea98ebc8afe34817371c7a3334c1473f49c25
Alexandre de Oliveira <chavedomundo@gmail.com>
2016-01-29 21:35:04 -0200

    First commit

---
+++ lib/ud/sync/rails.rb
@@ -0,0 +1,9 @@
+require "ud/sync/rails/version"
+
+module Ud
+  module Sync
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ud/sync/rails/version.rb
@@ -0,0 +1,7 @@
+module Ud
+  module Sync
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
